d1500cd1f93707e2f5384feaded101befe137e60
Pavel <Pavel@ZENBOOK14>
2023-10-21 15:57:41 +0200

    Přidejte soubory projektu.

---
+++ Ukazka - Sipka/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Ukazka___Sipka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(702, 406);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Zastavit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Ukazka - Sipka/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Projekt___Sipka;
+namespace Ukazka___Sipka
+{
+    public partial class Form1 : Form
+    {
+        Random rand;
+        Sipka[] sipky = new Sipka[10];
+        public Form1()
+        {
+            VytvorSipky();
+            InitializeComponent();
+            timer1.Start();
+        }
+        void VytvorSipky()
+        {
+            rand = new Random();
+            for(int i = 0; i < sipky.Length; i++)
+            {
+                int x = rand.Next(40, 150);
+                sipky[i] = new Sipka
+                {
+                    Parent = this,
+                    Size = new Size(x,x),
+                    Location = new Point(rand.Next(0, this.Width - Width), rand.Next(0, this.Height - Height)),
+                    
+                };
+                sipky[i].Naraz += Sipky_Naraz;
+                System.Threading.Thread.Sleep(1);
+            }
+        }
+
+        private void Sipky_Naraz(object sender, EventArgs e)
+        {
+            Sipka sipka = sender as Sipka;
+            rand = new Random();
+            int puvodniSmer = (int)sipka.Smer;
+            int novySmer;
+            do
+            {
+                novySmer = rand.Next(1, 5);
+            } while (novySmer == puvodniSmer);
+            sipka.Smer = (Smer)novySmer;
+            sipka.Location = new Point(rand.Next(0, this.Width - sipka.Width), rand.Next(0, this.Height - sipka.Height));
+            sipka.Refresh();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach(Sipka sipka in sipky)
+            {
+                sipka.Pohyb();
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+                button1.Text = "Spustit";
+            }
+            else
+            {
+                timer1.Start();
+                button1.Text = "Zastavit";
+            }
+        }
+    }
+}
